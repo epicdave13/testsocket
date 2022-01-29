7cc787705d9d42d3c6a48914b24dc55baee5bc8c
epicdave13 <98636081+epicdave13@users.noreply.github.com>
2022-01-29 10:51:00 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -3,14 +3,14 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = input('ip: ')
 port = 65000
-client.connect((ip,port))
+client.connect((ip, port))
 while True:
     data = client.recv(1024)
-    if data.decode('utf-8').split()[0] == '#IMAGE#':
-        with open(f"{data.decode('utf-8').split()[0]}",'ab') as img:
+    if data.decode('utf-8').split()[0] == 'img':
+        with open(f"{data.decode('utf-8').split()[1]}", 'ab') as img:
             imgData = client.recv(1024)
-                while imgData:
-                    img.write(imgData)
-                    imgData = client.recv(1024)
-                
+            while imgData:
+                img.write(imgData)
+                imgData = client.recv(1024)
+
     print(data.decode('utf-8'))
