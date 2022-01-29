02dd9c8ce708ef5996cd864d5ab1063628e74131
epicdave13 <98636081+epicdave13@users.noreply.github.com>
2022-01-29 10:34:19 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -6,4 +6,11 @@ port = 65000
 client.connect((ip,port))
 while True:
     data = client.recv(1024)
+    if data.decode('utf-8').split()[0] == '#IMAGE#':
+        with open(f"{data.decode('utf-8').split()[0]}",'ab') as img:
+            imgData = client.recv(1024)
+                while imgData:
+                    img.write(imgData)
+                    imgData = client.recv(1024)
+                
     print(data.decode('utf-8'))
